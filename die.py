727180bc2913f1e8ce3efcf7653317760a3fbdff
Din Becevic <dinbecevic@Din-Becevics-iMac.local>
2015-12-30 13:57:03 -0500

    Added testerino.js

--- die.py
+++ die.py
@@ -5,8 +5,12 @@ print die(3)
 
 print "k  i  l  l    m  e . . ."
 
+
 def bugfix(bug):
 	return bug * 0
 
 print bugfix(30)
 print "l o v i n g   l i f e . . . "
+
+print "s i n g l e   d a d   l o v i n g   l i f e. . ."
+
---
+++ testerino.js
@@ -0,0 +1,27 @@
+<html>
+	<body>
+
+	<script type="text/javascript">
+		<!--
+			var money = 35;
+			var costs = 60;
+
+			if(costs > money){
+				document.write("You need more money my dude!")
+			}
+			else{
+				document.write("You're looking good with money my man!")
+			}
+
+		var count:
+		document.write("Starting Loop" + "<br />");
+
+		for(count = 0: count < 10; count++){
+			document.write("Current Count : " + count );
+			document.write("<br />");
+		}
+
+		//-->
+		</script>
+	</body>
+</html>
