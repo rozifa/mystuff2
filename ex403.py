8c3e6e177031fb16646b4dc844f182f44ef69b68
rozifa <dinbecevic@gmail.com>
2016-01-05 02:16:58 -0500

    Added my self made animals class and excercise

---
+++ ex403.py
@@ -0,0 +1,40 @@
+class Animals(object):
+
+	def __init__(self, name, species, hair, likeable):
+		self.name = name
+		self.hair = hair
+		self.likeable = likeable
+		self.species = species
+
+	def get_name(self):
+		return self.name
+
+	def get_hair(self):
+		return self.hair
+
+	def get_likeable(self):
+		return self.likeable
+
+	def get_species(self):
+		return self.species
+
+# Create the animals (objects)
+
+mrmeow = Animals('Mr.Meow', 'Cat', 'Short-hair', 'He is ok')
+fudgems = Animals('Fudgems', 'Cat', 'Long and fluffy', 'One of the best')
+pistachio = Animals('Pistachio', 'Cat', "Nice N' Short", 'As good as Fudgems')
+bingus = Animals('Bingus', 'Dog', 'Spotted', 'He nice')
+
+meow_one = [mrmeow.get_name(), mrmeow.get_species(), mrmeow.get_hair(), mrmeow.get_likeable()]
+meow_two = [fudgems.get_name(), fudgems.get_species(), fudgems.get_hair(), fudgems.get_likeable()]
+meow_three = [pistachio.get_name(), pistachio.get_species(), pistachio.get_hair(), pistachio.get_likeable()]
+pupper = [bingus.get_name(), bingus.get_species(), bingus.get_hair(), bingus.get_likeable()]
+
+statstr = "This animal's stats are: "
+
+print statstr, meow_one
+print statstr, meow_two
+print statstr, meow_three
+print statstr, pupper
+
+print "WOW! I understand how classes and objects work in Python now...I think."
